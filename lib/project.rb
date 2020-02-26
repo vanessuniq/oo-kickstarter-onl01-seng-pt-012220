1f3c02cf13edb4bd3724c573762da0aeaca345c5
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-26 05:57:42 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_accessor :backer
+  attr_reader :backers, :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self)
+  end
+  
+end
